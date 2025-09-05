500ffb763e778b2a8c14a7e923b5dcae97f7f548
Koena Mahajan <96300463+KoenaMahajan@users.noreply.github.com>
2025-09-05 21:05:57 +0530

    Update CorsConfig.java

--- amusement-backend/src/main/java/com/amusement/amusement_park/config/CorsConfig.java
+++ amusement-backend/src/main/java/com/amusement/amusement_park/config/CorsConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+@Configuration // -> Spring annotation, tells the framework: “This class defines configuration beans.
+public class CorsConfig { //We are making a Spring configuration class called CorsConfig.
 
     @Bean
+    //Marks this method as a bean factory. The return value (WebMvcConfigurer object) will be registered in the Spring container.
     public WebMvcConfigurer corsConfigurer() {
+        //method that returns a WebMvcConfigurer implementation.
+        //Just the name of this bean (same as method name).
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
